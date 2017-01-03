4e441608288940fb387d22f8d41c74e68c3bbaab
jedmundo <joao.a.edmundo@gmail.com>
2017-01-03 11:05:19 +0100

    file type enum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,18 @@ import { Component, OnInit, NgZone } from '@angular/core';
 const { remote, ipcRenderer, shell } = electron;
 
 const videoAllowedExtensions: string[] = ['mp4', 'm4v', 'mkv'];
+const allAllowedExtensions: string[] = videoAllowedExtensions.concat(['.png', 'jpg', 'jpeg','pptx', '']);
+
+enum FILE_TYPE {
+    VIDEO,
+    DEFAULT
+}
 
 export class File {
     constructor(
         public path: string,
-        public previewPath: string,
-        public type: string = 'file'
+        public type: FILE_TYPE = FILE_TYPE.DEFAULT,
+        public thumbnailPath?: string
     ) {}
 }
 
@@ -21,6 +27,7 @@ export class AppComponent implements OnInit {
 
     public title = 'Plurch';
     public files: File[] = [];
+    public FILE_TYPE = FILE_TYPE;
 
     constructor(private zone: NgZone) {
     }
@@ -66,26 +73,22 @@ export class AppComponent implements OnInit {
                         this.zone.run(() => {
                             files.forEach(file => {
                                 if (this.isAllowedVideo(file)) {
-                                    this.addFileToGallery(itemPath + '/' + file, 'video');
+                                    this.addFile(itemPath + '/' + file, FILE_TYPE.VIDEO);
                                 } else {
-                                    this.addFileToGallery(itemPath);
+                                    this.addFile(itemPath, FILE_TYPE.DEFAULT);
                                 }
                             });
                         });
                     })
                 } else {
                     if (this.isAllowedVideo(itemPath)) {
-                        this.addFileToGallery(itemPath, 'video');
+                        this.addFile(itemPath, FILE_TYPE.VIDEO);
                     } else {
-                        this.addFileToGallery(itemPath);
+                        this.addFile(itemPath, FILE_TYPE.DEFAULT);
                     }
                 }
             });
         }
-    }
-
-    private addFileToGallery(path: string, type: string = 'file') {
-        this.addFile(path, 'files', type, this.files);
     }
 
     private isAllowedVideo(itemPath: string): boolean {
@@ -96,31 +99,40 @@ export class AppComponent implements OnInit {
         }
     }
 
-    private addFile(path: string, key: string, type: string, list: File[]): void {
+    private addFile(path: string, type: FILE_TYPE): void {
         console.log('ADD File:', path);
-        // ipcRenderer.on('save-preview-reply', (event, previewPath) => {
-        //     console.log(previewPath);
-        //     list.push(new File(path, previewPath));
+        // ipcRenderer.on('save-preview-reply', (event, thumbnailPath) => {
+        //     console.log(thumbnailPath);
+        //     list.push(new File(path, thumbnailPath));
         //     localStorage.setItem(key, JSON.stringify(list));
         // });
         // ipcRenderer.send('save-preview', path);
         const pathList = path.split('/');
-        const targetPath = '/Users/jedmundo/Desktop/plurch/thumbnails/preview_'
+        const thumbnailPath = '/Users/jedmundo/Desktop/plurch/thumbnails/preview_'
             + pathList[pathList.length-1].split('.')[0] + '.gif';
 
-        if (!filepreview.generateSync(path, targetPath)) {
-            console.log('Oops, something went wrong.');
+        if (type === FILE_TYPE.VIDEO) {
+            this.storeFile(path, type, thumbnailPath);
         } else {
-            list.push(new File(path, targetPath, type));
-            localStorage.setItem(key, JSON.stringify(list));
+            if (!filepreview.generateSync(path, thumbnailPath)) {
+                console.log('Oops, something went wrong.');
+                this.storeFile(path, type);
+            } else {
+                this.storeFile(path, type, thumbnailPath);
+            }
         }
+    }
+
+    private storeFile(path: string, type: FILE_TYPE, thumbnailPath?: string): void {
+        this.files.push(new File(path, type, thumbnailPath));
+        localStorage.setItem('files', JSON.stringify(this.files));
     }
 
     private loadItems(key: string, list: File[]): void {
         const fileList: File[] = JSON.parse(localStorage.getItem(key));
         if (fileList) {
             fileList.forEach((file: File) => {
-                list.push(new File(file.path, file.previewPath, file.type));
+                list.push(new File(file.path, file.type, file.thumbnailPath));
             });
         }
     }
