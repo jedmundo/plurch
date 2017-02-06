20eda95c914815a4e59d799c1db0c84194af8351
jedmundo <joao.a.edmundo@gmail.com>
2017-02-06 22:49:22 +0100

    video component - controls all out

--- src/app/day-schedule/video-item/video-item.component.ts
+++ src/app/day-schedule/video-item/video-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
 import { PlayableItem, VIDEO_COMMAND_TYPE } from '../day-schedule.component';
 import { WindowManagementService } from '../../shared/services/window-management.service';
 
@@ -7,9 +7,13 @@ import { WindowManagementService } from '../../shared/services/window-management
     templateUrl: 'video-item.component.html',
     styleUrls: ['video-item.component.scss']
 })
-export class VideoItemComponent implements OnInit {
+export class VideoItemComponent implements OnInit, AfterViewInit {
 
-    @ViewChild('videoPlayer') videoplayer: any;
+    @ViewChild('videoPlayer') private videoplayer: any;
+    @ViewChild('seekBar') private seekBar: any;
+    @ViewChild('volumeBar') private volumeBar: any;
+    public videoPlayerElement;
+    public currentVideoTime;
 
     public isVideoPaused: boolean = true;
     public VIDEO_COMMAND_TYPE = VIDEO_COMMAND_TYPE;
@@ -19,6 +23,11 @@ export class VideoItemComponent implements OnInit {
     constructor(private windowManagementService: WindowManagementService) { }
 
     public ngOnInit() {
+
+    }
+
+    public ngAfterViewInit(): void {
+        this.videoPlayerElement = this.videoplayer.nativeElement;
         this.videoLoaded();
     }
 
@@ -27,14 +36,14 @@ export class VideoItemComponent implements OnInit {
 
         switch (command) {
             case VIDEO_COMMAND_TYPE.PLAY:
-                this.videoplayer.nativeElement.play();
+                this.videoPlayerElement.play();
                 break;
             case VIDEO_COMMAND_TYPE.PAUSE:
-                this.videoplayer.nativeElement.pause();
+                this.videoPlayerElement.pause();
                 break;
             case VIDEO_COMMAND_TYPE.RESTART:
                 this.isVideoPaused = true;
-                this.videoplayer.nativeElement.load();
+                this.videoPlayerElement.load();
                 break;
         }
 
@@ -43,43 +52,39 @@ export class VideoItemComponent implements OnInit {
 
     private videoLoaded(): void {
         console.log('LISTENERS ADDED');
-        this.videoplayer.nativeElement.addEventListener("seeking", () => {
+        this.videoPlayerElement.addEventListener("seeking", () => {
             this.windowManagementService.sendMessageToWindow(123, 'send-video-type',
-                { type: VIDEO_COMMAND_TYPE.SYNC_TIME, value: this.videoplayer.nativeElement.currentTime });
+                { type: VIDEO_COMMAND_TYPE.SYNC_TIME, value: this.videoPlayerElement.currentTime });
         });
 
-        this.videoplayer.nativeElement.addEventListener("seeked", () => {
+        this.videoPlayerElement.addEventListener("seeked", () => {
             this.windowManagementService.sendMessageToWindow(123, 'send-video-type',
-                { type: VIDEO_COMMAND_TYPE.SYNC_TIME, value: this.videoplayer.nativeElement.currentTime });
+                { type: VIDEO_COMMAND_TYPE.SYNC_TIME, value: this.videoPlayerElement.currentTime });
         });
 
-        this.videoplayer.nativeElement.addEventListener("timeupdate", () => {
-            // Calculate the slider value
-            const value = (100 / this.videoplayer.nativeElement.duration) * this.videoplayer.nativeElement.currentTime;
-            const seekBar: HTMLInputElement = <HTMLInputElement> document.getElementById("seek-bar");
-            // Update the slider value
-            seekBar.value = <any> value;
+        this.videoPlayerElement.addEventListener("timeupdate", () => {
+            const value = (100 / this.videoPlayerElement.duration) * this.videoPlayerElement.currentTime;
+            this.currentVideoTime = this.videoPlayerElement.currentTime;
+            this.seekBar.nativeElement.value = <any> value;
         });
 
-        // this.videoplayer.nativeElement.addEventListener('loadedmetadata', function() {
-        //     console.log(this.videoplayer.nativeElement.duration);
+        // this.videoPlayerElement.addEventListener('loadedmetadata', function() {
+        //     console.log(this.videoPlayerElement.duration);
         // });
     }
 
     public muteVideo(): void {
-        this.videoplayer.nativeElement.muted = !this.videoplayer.nativeElement.muted;
+        this.videoPlayerElement.muted = !this.videoPlayerElement.muted;
     }
 
     public videoSeekChange(): void {
-        const seekBar: HTMLInputElement = <HTMLInputElement> document.getElementById("seek-bar");
-        const time = this.videoplayer.nativeElement.duration * (<any> seekBar.value / 100);
+        const time = this.videoPlayerElement.duration * (<any> this.seekBar.nativeElement.value / 100);
         // Update the video time
-        this.videoplayer.nativeElement.currentTime = time;
+        this.videoPlayerElement.currentTime = time;
     }
 
     public volumeChange(): void {
-        const volumeBar: HTMLInputElement = <HTMLInputElement> document.getElementById('volume-bar');
-        this.videoplayer.nativeElement.volume = <any> volumeBar.value;
+        this.videoPlayerElement.volume = <any> this.volumeBar.nativeElement.value;
     }
 
 }
