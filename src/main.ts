32a344533621bad9b5dcb5d03f76ba17cf0cecac
jedmundo <joao.a.edmundo@gmail.com>
2017-02-25 13:53:41 +0100

    program in dialog done

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
+import { IS_DEBUG } from './app/app.component';
 
-if (environment.production) {
+// if (environment.production) {
+//   enableProdMode();
+// }
+
+if (!IS_DEBUG) {
   enableProdMode();
 }
 
