6290aea4e07866b389793ba44460b86d19c00cf2
jedmundo <joao.a.edmundo@gmail.com>
2017-05-13 11:54:57 +0200

    program height with a max

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,8 +1,8 @@
 // Typings reference file, you can add your own global typings here
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
-declare var electron: any;
-declare var fs: any;
-declare var path: any;
-declare var ytdl: any;
-declare var loudness: any;
+declare let electron: any;
+declare let fs: any;
+declare let path: any;
+declare let ytdl: any;
+declare let loudness: any;
